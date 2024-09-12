d7544d6e310e9321fe90f8586535bb02606150fd
Cezarelus <pcezar.2378.st@gmail.com>
2024-09-12 23:10:25 +0300

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,10 @@
+﻿namespace Pinci_Catalog
+{
+    internal class Account
+    {
+        public String User { get; set; }
+        public String Password { get; set; }
+
+        public int UserType { get; set; }
+    }
+}
---
+++ Aplicatie.cs
@@ -0,0 +1,14 @@
+﻿namespace Pinci_Catalog
+{
+    internal static class Aplicatie
+    {
+     
+        [STAThread]
+        static void Main()
+        {
+           
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
